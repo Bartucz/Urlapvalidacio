142d00234a6043713810c7bfcbf92c8ed63cde0f
Bartucz <43059630+Bartucz@users.noreply.github.com>
2021-04-23 15:54:04 +0200

    Kész
    Kész ,üzenet mező nélkül

--- Urlapvalidacio/public_html/urlapvalidacio.js
+++ Urlapvalidacio/public_html/urlapvalidacio.js
@@ -37,6 +37,9 @@ function validalas() {
     }
     //https://regexone.com/
     var szuro = /[A-Z]+[a-z]{2,}/;
+    var emailszuro=/[a-z]{2,}\.[a-z]{2,}\@[a-z]{2,}\.[a-z]/;
+    var telszuro=/\+36\-\d{2}\-\d{3}\-\d{4}/;
+    var webszuro=/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;//https://www.regextester.com/93652
 
     if (!szuro.test(nevInput)) {
         hiba += "Nagybetüvel kezdődjön!";
@@ -47,7 +50,37 @@ function validalas() {
         ID("nev").style.border = "none";
     }
 
-
+    if(!emailszuro.test(ID("email1").value)){
+        hiba+="Hibás email formátum: valaki@valahol.hu, minden elem min. 3 karakter!";
+        ID("email1").style.border="3px solid red";
+    }else{
+        ID("email1").style.border="none";
+        
+    }
+    //email egyezes
+    if((ID("email2").value!==ID("email1").value)){
+        hiba+="Nem azonos az e-mail!";
+        ID("email2").style.border="3px solid red";
+    }else{
+        ID("email2").style.border="none";
+        urlapAdatok+="Email: "+ID("email1").value+"<br>";
+    }
+    //tel
+    if(!telszuro.test(ID("tel").value)){
+        hiba+="Nem helyes a formátum' (+36-30-000-0000)";
+        ID("tel").style.border="3px solid red";
+    }else{
+        ID("tel").style.border="none";
+        urlapAdatok+="Tel: "+ID("tel").value+"<br>";
+    }
+    //weboldal
+    if(!webszuro.test(ID("url").value)){
+        hiba+="Nem helyes a formátum' (http://valami.hu)";
+        ID("url").style.border="3px solid red";
+    }else{
+        ID("url").style.border="none";
+        urlapAdatok+="Webcím: "+ID("url").value+"<br>";
+    }
 
     $("aside section:nth-child(1) p")[0].innerHTML = hiba;
     $("aside section:nth-child(2) p")[0].innerHTML = urlapAdatok;
